bef4321eb5b1f8c5b911317aace60455bc78123d
Veikko Kovanen <veikko.kovanen@gmail.com>
2022-05-10 09:21:36 +0300

    refactor: implement only used parts of paraswap interface

--- contracts/adapters/paraswapV5/ParaswapV5Adapter.sol
+++ contracts/adapters/paraswapV5/ParaswapV5Adapter.sol
@@ -2,33 +2,31 @@
 pragma solidity 0.8.4;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import './interfaces/Utils.sol';
-import './interfaces/IAugustusSwapper.sol';
-import './interfaces/IParaswap.sol';
 import '../BaseAdapter.sol';
+import {IAugustusSwapper, SimpleData, SellData} from './interfaces/IParaswap.sol';
 
 contract ParaswapV5Adapter is BaseAdapter {
-  function simpleSwap(address _augustusSwapper, bytes calldata _data) external payable {
+  function simpleSwap(address _router, bytes calldata _data) external payable {
     HousecatManagement mgmt = _getMgmt();
-    require(mgmt.isIntegrationSupported(_augustusSwapper), 'ParaswapV5Adapter: unsupported router');
-    Utils.SimpleData memory data = abi.decode(_data[4:], (Utils.SimpleData));
+    require(mgmt.isIntegrationSupported(_router), 'ParaswapV5Adapter: unsupported router');
+    SimpleData memory data = abi.decode(_data[4:], (SimpleData));
     require(mgmt.isAssetSupported(data.toToken), 'ParaswapV5Adapter: unsupported token to');
     data.partner = payable(address(0));
-    address tokenTransferProxy = IAugustusSwapper(_augustusSwapper).getTokenTransferProxy();
+    address tokenTransferProxy = IAugustusSwapper(_router).getTokenTransferProxy();
     IERC20(data.fromToken).approve(tokenTransferProxy, data.fromAmount);
-    IParaswap(_augustusSwapper).protectedSimpleSwap(data);
+    IAugustusSwapper(_router).protectedSimpleSwap(data);
   }
 
-  function multiSwap(address _augustusSwapper, bytes calldata _data) external payable {
+  function multiSwap(address _router, bytes calldata _data) external payable {
     HousecatManagement mgmt = _getMgmt();
-    require(mgmt.isIntegrationSupported(_augustusSwapper), 'ParaswapV5Adapter: unsupported router');
-    Utils.SellData memory data = abi.decode(_data[4:], (Utils.SellData));
+    require(mgmt.isIntegrationSupported(_router), 'ParaswapV5Adapter: unsupported router');
+    SellData memory data = abi.decode(_data[4:], (SellData));
     address toToken = data.path[data.path.length - 1].to;
     require(mgmt.isAssetSupported(toToken), 'ParaswapV5Adapter: unsupported token to');
     data.partner = payable(address(0));
     data.deadline = block.timestamp;
-    address tokenTransferProxy = IAugustusSwapper(_augustusSwapper).getTokenTransferProxy();
+    address tokenTransferProxy = IAugustusSwapper(_router).getTokenTransferProxy();
     IERC20(data.fromToken).approve(tokenTransferProxy, data.fromAmount);
-    IParaswap(_augustusSwapper).protectedMultiSwap(data);
+    IAugustusSwapper(_router).protectedMultiSwap(data);
   }
 }
