9a788f2c250a1efcf057fb09aa29e00f50399cf6
Veikko Kovanen <veikko.kovanen@gmail.com>
2022-05-21 15:16:38 +0300

    fx import path

--- utils/get-deployment.ts
+++ utils/get-deployment.ts
@@ -2,7 +2,7 @@ import hre from 'hardhat'
 
 const getDeployment = () => {
   const network = hre.hardhatArguments.network
-  return require(`../../deployment-${network}.json`)
+  return require(`../deployment-${network}.json`)
 }
 
 export default getDeployment
